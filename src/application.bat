c5907bb554fde24d80edcf199be636bf8772c4a4
Yoann Bescher-Leblanc <yoann.bescher-leblanc@laposte.net>
2013-10-11 14:44:15 +0200

    update header for new member

--- src/application.bat
+++ src/application.bat
@@ -1,6 +1,8 @@
-### Application.bat - 2013 - Huve ###
+### Application.bat - 2013 - Huve
+Modif: BESCHER-LEBLANC Yoann ###
 "version 2"
 build_hour = "11H00"
 
 echo "Build ${build_hour}"
 echo "version ${version}"
+
